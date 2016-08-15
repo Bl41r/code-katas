e1147f614ac05ec2808f5b9fecbd0dbf9a524439
David Smith <dbsmith.dbs83@gmail.com>
2016-08-14 17:21:26 -0700

    done

--- Sum-of-the-first-nth-term-of-Series.py
+++ Sum-of-the-first-nth-term-of-Series.py
@@ -7,6 +7,5 @@ http://www.codewars.com/kata/sum-of-the-first-nth-term-of-series/train/python
 def series_sum(n):
     sum = 0.00
     for i in range(1, n + 1):
-        print((1.0 / (1 + 3 * (i - 1))))
         sum += (1.0 / (1 + 3 * (i - 1)))
     return '%.2f' % sum
